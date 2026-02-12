e3a1f137b0b56b28a66475838dfefd93f54a8ee4
ExtremeXT <extremextdev@gmail.com>
2026-02-12 14:29:26 +0200

    extra: Cleanup
    Change-Id: I0b396145ee7056ab905ca260381541f5b2ecbf6f
    Signed-off-by: ExtremeXT <extremextdev@gmail.com>

--- BoardConfigExtra.mk
+++ BoardConfigExtra.mk
@@ -1,16 +1,4 @@
-# Product
-ifndef TARGET_COPY_OUT_PRODUCT
-    TARGET_COPY_OUT_PRODUCT := system/product
+# GApps
+ifeq ($(WITH_GMS), true)
+-include vendor/gapps/arm64/BoardConfigVendor.mk
 endif
-
-# Recovery
-#BOARD_USES_FULL_RECOVERY_IMAGE ?= true
-
-# System_ext
-ifndef TARGET_COPY_OUT_SYSTEM_EXT
-    TARGET_COPY_OUT_SYSTEM_EXT := system/system_ext
-endif
-
-## SELinux
-PRODUCT_PRIVATE_SEPOLICY_DIRS += vendor/extra/sepolicy/private
-BOARD_VENDOR_SEPOLICY_DIRS += vendor/extra/sepolicy/vendor
--- product.mk
+++ product.mk
@@ -1,70 +1,30 @@
-## ADB Keys
-
-# Android
+# ADB Keys
 PRODUCT_COPY_FILES += \
-    vendor/extra/adb_keys:root/adb_keys
-
-# Recovery
-PRODUCT_COPY_FILES += \
+    vendor/extra/adb_keys:root/adb_keys \
     vendor/extra/adb_keys:recovery/root/adb_keys
 
-## Bash
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    persist.sys.adb.shell=/system_ext/bin/bash
+PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
+    root/adb_keys
 
-## Debugging
-ifeq ($(filter %foster %foster_tab %porg %porg_tab %sif %sphynx %nx %nx_tab,$(TARGET_PRODUCT)),)
-ifneq ($(TARGET_BUILD_VARIANT),eng)
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += log.tag=I
-endif
+# GApps
+ifeq ($(WITH_GMS), true)
+$(call inherit-product-if-exists, vendor/gapps/arm64/arm64-vendor.mk)
 endif
 
-## ih8sn
+# ih8sn
 PRODUCT_PACKAGES += ih8sn
 
 PRODUCT_COPY_FILES += \
     vendor/extra/configs/ih8sn/ih8sn_generic.conf:/system/etc/ih8sn.conf
-## Logging
-$(call soong_config_set,liblog,force_debuggable,true)
-
-## neofetch
-PRODUCT_COPY_FILES += \
-    vendor/extra/tools/neofetch:$(TARGET_COPY_OUT_SYSTEM_EXT)/bin/neofetch
-
-## Overlays
-PRODUCT_PACKAGE_OVERLAYS += vendor/extra/overlay
-
-## Recovery
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += persist.vendor.recovery_update=true
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += persist.vendor.recovery_allow_spl_downgrade=true
-
-## SUW
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    setupwizard.feature_deferred_snooze_allow_never=true
-
-#ifeq ($(WITH_GMS),true)
-#ifneq ($(PRODUCT_IS_ATV),true)
-#ifneq ($(PRODUCT_IS_AUTOMOTIVE),true)
-#$(call inherit-product, vendor/google_pixel/products/google_pixel.mk)
-#endif # PRODUCT_IS_AUTOMOTIVE
-#endif # PRODUCT_IS_ATV
-#endif # WITH_GMS
 
 PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
-    root/adb_keys \
     system/bin/ih8sn \
     system/etc/ih8sn.conf \
     system/etc/init/ih8sn.rc
 
-# Enable whole-program R8 Java optimizations for SystemUI and system_server,
-# but also allow explicit overriding for testing and development.
-SYSTEM_OPTIMIZE_JAVA ?= true
-SYSTEMUI_OPTIMIZE_JAVA ?= true
+# Recovery
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += persist.vendor.recovery_update=true
 
-# Preopt SystemUI
-PRODUCT_DEXPREOPT_SPEED_APPS += SystemUI Settings
-
-# Compile SystemUI on device with `speed`.
-PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.systemuicompilerfilter=speed
-
+# SUW
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    setupwizard.feature_deferred_snooze_allow_never=true
