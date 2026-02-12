9e3bc11076d8d875050b77ddb22058ee0675345b
ExtremeXT <extremextdev@gmail.com>
2026-02-12 19:34:33 +0200

    extra: Kill ih8sn
    Kills Revolut.
    
    Change-Id: I90f49768f94a0cabd3b92e0387487e157752ddb4
    Signed-off-by: ExtremeXT <extremextdev@gmail.com>

--- product.mk
+++ product.mk
@@ -11,17 +11,6 @@ ifeq ($(WITH_GMS), true)
 $(call inherit-product-if-exists, vendor/gapps/arm64/arm64-vendor.mk)
 endif
 
-# ih8sn
-PRODUCT_PACKAGES += ih8sn
-
-PRODUCT_COPY_FILES += \
-    vendor/extra/configs/ih8sn/ih8sn_generic.conf:/system/etc/ih8sn.conf
-
-PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
-    system/bin/ih8sn \
-    system/etc/ih8sn.conf \
-    system/etc/init/ih8sn.rc
-
 # Recovery
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += persist.vendor.recovery_update=true
 
